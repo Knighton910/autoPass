1b7bc687ed413cafa7ecdc893d0207d2c375d362
🦁 Kelvin-Knighton <keltheceo@gmail.com>
2018-07-02 11:42:18 -0700

    poured all the code from ava, but some styles are err

---
+++ src/app/car.model.ts
@@ -0,0 +1,14 @@
+export class Car {
+  constructor(
+    public imageUrl: string,
+    public name: string,
+    public price: string,
+    public topSpeed: string,
+    public msrp: string,
+    public info: string,
+    public stat1: string,
+    public stat2: string,
+    public stat3: string,
+    public id: number
+  ) {}
+}
